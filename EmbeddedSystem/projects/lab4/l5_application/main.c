8119c8e030b471eddd526634c05f58ca62f11e82
bang nguyen <pherobang.nguyen@gmail.com>
2020-10-10 00:51:37 -0700

    Update lab4_ADC_PWM

--- EmbeddedSystem/projects/lab4/l5_application/main.c
+++ EmbeddedSystem/projects/lab4/l5_application/main.c
@@ -1,105 +1,157 @@
-#include <stdio.h>
-
 #include "FreeRTOS.h"
-#include "task.h"
-
+#include "adc.h"
 #include "board_io.h"
 #include "common_macros.h"
+#include "delay.h"
+#include "gpio.h"
+#include "gpio_isr.h"
+#include "gpio_lab.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
 #include "periodic_scheduler.h"
+#include "pwm1.h"
+#include "queue.h"
+#include "semphr.h"
 #include "sj2_cli.h"
+#include "task.h"
+#include <stdio.h>
 
-// 'static' to make these functions 'private' to this file
-static void create_blinky_tasks(void);
-static void create_uart_task(void);
-static void blink_task(void *params);
-static void uart_task(void *params);
+/************************************PWM Task (Part 0)****************************************
+*@brief:    PWM Task with 3 Channel control RBG LED
+*@para:     No Parameter
+*@return:   No return
+*@Note:     We only need to set PWM configuration once, and the HW will drive
+            Expected Output:
+            -->Will Dynamicly Change Duty Cycle --> Control RED BLUE GREEN Ratio
+ ********************************************************************************************/
+void pwm_task(void *p) {
+  pwm1__init_single_edge(1000);
 
+  /* Enable PWM */
+  pin_configure_pwm_channel_as_io_pin(2, 0);
+  pin_configure_pwm_channel_as_io_pin(2, 1);
+  pin_configure_pwm_channel_as_io_pin(2, 2);
+
+  uint8_t percent = 0;
+  while (1) {
+    /* Read MR0 and MR1 Value as Requirement */
+    uint32_t match0 = LPC_PWM1->MR0 & 0xFFFFFFFF;
+    uint32_t match1 = LPC_PWM1->MR1 & 0xFFFFFFFF;
+    fprintf(stderr, "MR0: %d MR1: %d \n", match0, match1);
+    /* Set Duty Cycle for 3 Channel */
+    pwm1__set_duty_cycle(PWM1__2_0, percent);
+    pwm1__set_duty_cycle(PWM1__2_1, 100 - percent);
+    pwm1__set_duty_cycle(PWM1__2_2, percent / 2);
+
+    if (++percent > 100) {
+      percent = 0;
+    }
+
+    vTaskDelay(100);
+  }
+}
+
+/***************************************ADC Task (Part 1)***************************************
+*@brief:    adc_task()-Software + adc_taskBurst()-Hardware
+*@para:     No Parameter
+*@return:   No return
+*@Note:     adc_task()        -->Print out RAW Data
+            adc_taskBurst()   -->Voltage
+ ***********************************************************************************************/
+void adc_task(void *p) {
+  /* Enable IOPIN + Initialize ADC */
+  adc_enable_P1_31();
+  adc__initialize();
+
+  while (1) {
+    const uint16_t adc_value = adc__get_adc_value(ADC__CHANNEL_5);
+    fprintf(stderr, "ADC Result: %X \n", adc_value);
+    vTaskDelay(100);
+  }
+}
+
+void adc_taskBurst(void *p) {
+  /* Enable IOPIN + Initialize ADC + Burst Enable */
+  adc_enable_P1_31();
+  adc__initialize();
+  adc__enable_burst_mode(ADC__CHANNEL_5);
+
+  while (1) {
+    const uint16_t adc_value = adc__get_adc_value_burst(ADC__CHANNEL_5);
+    fprintf(stderr, "ADC Bust Mode Result: %f V \n", ((double)adc_value / 4095) * 3.3);
+    vTaskDelay(100);
+  }
+}
+
+/************************************ADC + PWM (Part 2)******************************************
+*@brief:    Using RTOS Queue to Perform Part1 -Part0
+*@para:     No Parameter
+*@return:   No return
+*@Note:     adc_taskBurst2()    --> get RAW_Value/4095 --> xQueueSend()
+            pwm_task2()         --> xQueueReceive(..,...,Delay) --> Control Duty Cycle
+            Expect Output:      --> Adjust POT ---> RBG change color
+ ***********************************************************************************************/
+static QueueHandle_t adc_to_pwm_task_queue;
+
+void adc_taskBurst2(void *p) {
+  adc_enable_P1_31();
+  adc__initialize();
+  adc__enable_burst_mode(ADC__CHANNEL_5);
+
+  while (1) {
+    const uint16_t adc_value = adc__get_adc_value_burst(ADC__CHANNEL_5);
+    xQueueSend(adc_to_pwm_task_queue, &adc_value, 0);
+    fprintf(stderr, "Send ADC: %f V\n", ((double)adc_value / 4095) * 3.3);
+    vTaskDelay(100);
+  }
+}
+/* =============================================================== */
+void pwm_task2(void *p) {
+  /* Set Frequency */
+  pwm1__init_single_edge(1000);
+
+  /* Enable PWM */
+  pin_configure_pwm_channel_as_io_pin(2, 0); // 2 - 0
+  pin_configure_pwm_channel_as_io_pin(2, 1);
+  pin_configure_pwm_channel_as_io_pin(2, 2);
+
+  int percent = 0;
+  uint16_t adc_value = 0;
+  while (1) {
+    /* Match register */
+    uint32_t match0 = LPC_PWM1->MR0 & 0xFFFFFFFF;
+    uint32_t match1 = LPC_PWM1->MR1 & 0xFFFFFFFF;
+    fprintf(stderr, "MR0: %d MR1: %d \n", match0, match1);
+    /* Receive potentiometer value from queue */
+    if (xQueueReceive(adc_to_pwm_task_queue, &adc_value, 1000)) {
+      fprintf(stderr, "PWM Received Value: %d\n", adc_value);
+      percent = (double)adc_value / 4095.0 * 100;
+      pwm1__set_duty_cycle(PWM1__2_0, percent);
+      pwm1__set_duty_cycle(PWM1__2_1, percent / 2);
+      pwm1__set_duty_cycle(PWM1__2_2, 100 - percent);
+      // vTaskDelay(100);
+    }
+  }
+}
+
+/***************************************** MAIN LOOP *********************************************
+**************************************************************************************************/
 int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
-
-  // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
-  // See esp32/README.md for more details
-  // uart3_init();                                                                     // Also include:  uart3_init.h
-  // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
 
   puts("Starting RTOS");
+
+  /******************************* Part 0 **************************** */
+  // xTaskCreate(pwm_task, "pwm Task", 1024, NULL, 1, NULL);
+
+  /******************************* Part 1 **************************** */
+  // xTaskCreate(adc_taskBurst, "adc Task", 1024, NULL, 1, NULL);
+
+  /***************************** Part 2 + 3 ************************** */
+  adc_to_pwm_task_queue = xQueueCreate(1, sizeof(uint16_t));
+  xTaskCreate(adc_taskBurst2, "adc Task", 1024 / sizeof(void *), NULL, 1, NULL);
+  xTaskCreate(pwm_task2, "pmw_task", 1024 / sizeof(void *), NULL, 2, NULL);
+
   vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
 
   return 0;
-}
-
-static void create_blinky_tasks(void) {
-  /**
-   * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
-   * Use '#if (0)' if you wish to use the 'periodic_scheduler.h' that will spawn 4 periodic tasks, one for each LED
-   */
-#if (1)
-  // These variables should not go out of scope because the 'blink_task' will reference this memory
-  static gpio_s led0, led1;
-
-  led0 = board_io__get_led0();
-  led1 = board_io__get_led1();
-
-  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
-  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
-#else
-  const bool run_1000hz = true;
-  const size_t stack_size_bytes = 2048 / sizeof(void *); // RTOS stack size is in terms of 32-bits for ARM M4 32-bit CPU
-  periodic_scheduler__initialize(stack_size_bytes, !run_1000hz); // Assuming we do not need the high rate 1000Hz task
-  UNUSED(blink_task);
-#endif
-}
-
-static void create_uart_task(void) {
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if (0)' to '#if (1)' and vice versa to try it out
-#if (0)
-  // printf() takes more stack space, size this tasks' stack higher
-  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-#else
-  sj2_cli__init();
-  UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
-#endif
-}
-
-static void blink_task(void *params) {
-  const gpio_s led = *((gpio_s *)params); // Parameter was input while calling xTaskCreate()
-
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
-  while (true) {
-    gpio__toggle(led);
-    vTaskDelay(500);
-  }
-}
-
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
-static void uart_task(void *params) {
-  TickType_t previous_tick = 0;
-  TickType_t ticks = 0;
-
-  while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
-    vTaskDelayUntil(&previous_tick, 2000);
-
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
-     *
-     * Use this style print for:
-     *  - Interrupts because you cannot use printf() inside an ISR
-     *    This is because regular printf() leads down to xQueueSend() that might block
-     *    but you cannot block inside an ISR hence the system might crash
-     *  - During debugging in case system crashes before all output of printf() is sent
-     */
-    ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
-    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
-
-    /* This deposits data to an outgoing queue and doesn't block the CPU
-     * Data will be sent later, but this function would return earlier
-     */
-    ticks = xTaskGetTickCount();
-    printf("This is a more efficient printf ... finished in");
-    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
-  }
 }
