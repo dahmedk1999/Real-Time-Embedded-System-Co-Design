7cc2831efcee0be9d96053c810ad76f9b15c03e5
bang nguyen <pherobang.nguyen@gmail.com>
2020-10-10 00:54:46 -0700

    Update lab5_SPI

--- EmbeddedSystem/projects/lab5/l5_application/main.c
+++ EmbeddedSystem/projects/lab5/l5_application/main.c
@@ -1,105 +1,178 @@
-#include <stdio.h>
-
 #include "FreeRTOS.h"
-#include "task.h"
-
+#include "adc.h"
 #include "board_io.h"
 #include "common_macros.h"
+#include "delay.h"
+#include "gpio.h"
+
+#include "gpio_lab.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
 #include "periodic_scheduler.h"
+
+#include "queue.h"
+#include "semphr.h"
 #include "sj2_cli.h"
+#include "ssp2_lab.h"
+#include "task.h"
+#include <stdio.h>
 
-// 'static' to make these functions 'private' to this file
-static void create_blinky_tasks(void);
-static void create_uart_task(void);
-static void blink_task(void *params);
-static void uart_task(void *params);
+/***************************************** Declaration *********************************************
+****************************************************************************************************/
+/* Adesto flash 'Manufacturer and Device ID' section */
+typedef struct {
+  uint8_t manufacturer_id;
+  uint8_t device_id_1;
+  uint8_t device_id_2;
+  uint8_t extended_device_id;
+} adesto_flash_id_s;
 
-int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
+adesto_flash_id_s FLASH_ID;
 
-  // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
-  // See esp32/README.md for more details
-  // uart3_init();                                                                     // Also include:  uart3_init.h
-  // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
+SemaphoreHandle_t spi_bus_mutex;
+/***************************************SSP2 Task (Part 1)***************************************
+ *@brief:   Read the adesto Flash memory Manufacture ID and Device ID
+ *@Note:    Create a ptr[4] array to copy the data out
+ ************************************************************************************************/
+void spi_task(void *p) {
 
-  puts("Starting RTOS");
-  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+  /* CLOCK = 1Mhz PreScale = 96*/
+  const uint32_t spi_clock_mhz = 1;
+  ssp2__setup(spi_clock_mhz);
 
-  return 0;
-}
+  /* P1_0-CLK | P1_1-MISO | P1_4-MOSI */
+  ssp2_PIN_config();
+  uint8_t ptr[4] = {0, 0, 0, 0};
 
-static void create_blinky_tasks(void) {
-  /**
-   * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
-   * Use '#if (0)' if you wish to use the 'periodic_scheduler.h' that will spawn 4 periodic tasks, one for each LED
-   */
-#if (1)
-  // These variables should not go out of scope because the 'blink_task' will reference this memory
-  static gpio_s led0, led1;
+  while (1) {
 
-  led0 = board_io__get_led0();
-  led1 = board_io__get_led1();
+    /* Start READ */
+    ssp2_read(0x9F, 4, ptr);
+    FLASH_ID.manufacturer_id = ptr[0];
+    FLASH_ID.device_id_1 = ptr[1];
+    FLASH_ID.device_id_2 = ptr[2];
+    FLASH_ID.extended_device_id = ptr[3];
 
-  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
-  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
-#else
-  const bool run_1000hz = true;
-  const size_t stack_size_bytes = 2048 / sizeof(void *); // RTOS stack size is in terms of 32-bits for ARM M4 32-bit CPU
-  periodic_scheduler__initialize(stack_size_bytes, !run_1000hz); // Assuming we do not need the high rate 1000Hz task
-  UNUSED(blink_task);
-#endif
-}
-
-static void create_uart_task(void) {
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if (0)' to '#if (1)' and vice versa to try it out
-#if (0)
-  // printf() takes more stack space, size this tasks' stack higher
-  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-#else
-  sj2_cli__init();
-  UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
-#endif
-}
-
-static void blink_task(void *params) {
-  const gpio_s led = *((gpio_s *)params); // Parameter was input while calling xTaskCreate()
-
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
-  while (true) {
-    gpio__toggle(led);
+    /*Errors handle*/
+    if (FLASH_ID.manufacturer_id != 0x1F) {
+      fprintf(stderr, "Manufacturer ID read failure\n");
+      vTaskSuspend(NULL); // Kill this task
+    } else {
+      fprintf(stderr, "Manufacture ID:  %X \n", FLASH_ID.manufacturer_id);
+      fprintf(stderr, "Device_ID1:      %X \n", FLASH_ID.device_id_1);
+      fprintf(stderr, "Device_ID2:      %X \n", FLASH_ID.device_id_2);
+      fprintf(stderr, "Manufacture ID:  %X \n", FLASH_ID.extended_device_id);
+      fprintf(stderr, "\n");
+    }
     vTaskDelay(500);
   }
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
-static void uart_task(void *params) {
-  TickType_t previous_tick = 0;
-  TickType_t ticks = 0;
+/***********************************SSP2 Mutex Task (Part 2b)************************************
+ *@brief:   Using one function to run two different task
+ *@para:    *Task1 = 1
+            *Task2 = 2
+ *@Note:    Task parameter will be past into xTaskCreate(....,...)
+            --> the demonstrate the purpose of Mutex vs Semaphore
+ ************************************************************************************************/
+static int *task1 = 1;
+static int *task2 = 2;
+void Mutex_ID_task(void *p) {
 
-  while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
-    vTaskDelayUntil(&previous_tick, 2000);
+  const int *task = (int *)(p);
 
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
-     *
-     * Use this style print for:
-     *  - Interrupts because you cannot use printf() inside an ISR
-     *    This is because regular printf() leads down to xQueueSend() that might block
-     *    but you cannot block inside an ISR hence the system might crash
-     *  - During debugging in case system crashes before all output of printf() is sent
-     */
-    ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
-    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
+  while (1) {
+    if (xSemaphoreTake(spi_bus_mutex, 1000)) {
 
-    /* This deposits data to an outgoing queue and doesn't block the CPU
-     * Data will be sent later, but this function would return earlier
-     */
-    ticks = xTaskGetTickCount();
-    printf("This is a more efficient printf ... finished in");
-    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
+      fprintf(stderr, "Task: %d \n", *task);
+      /* CLOCK = 1Mhz PreScale = 96*/
+      const uint32_t spi_clock_mhz = 1;
+      ssp2__setup(spi_clock_mhz);
+
+      /* P1_0-CLK | P1_1-MISO | P1_4-MOSI */
+      ssp2_PIN_config();
+      uint8_t ptr[4] = {0, 0, 0, 0};
+
+      /* Start READ */
+      ssp2_read(0x9F, 4, ptr);
+      FLASH_ID.manufacturer_id = ptr[0];
+      FLASH_ID.device_id_1 = ptr[1];
+      FLASH_ID.device_id_2 = ptr[2];
+      FLASH_ID.extended_device_id = ptr[3];
+
+      /*Errors handle*/
+      if (FLASH_ID.manufacturer_id != 0x1F) {
+        fprintf(stderr, "Manufacturer ID read failure\n");
+        vTaskSuspend(NULL); // Kill this task
+      } else {
+        fprintf(stderr, "Manufacture ID:  %X \n", FLASH_ID.manufacturer_id);
+        fprintf(stderr, "Device_ID1:      %X \n", FLASH_ID.device_id_1);
+        fprintf(stderr, "Device_ID2:      %X \n", FLASH_ID.device_id_2);
+        fprintf(stderr, "Manufacture ID:  %X \n", FLASH_ID.extended_device_id);
+        fprintf(stderr, "\n");
+      }
+      xSemaphoreGive(spi_bus_mutex);
+      vTaskDelay(500);
+    }
   }
+}
+
+/*******************************SSP2 Task Extra Credit (Part 2a)**********************************
+ *@brief:   Perform READ/WRITE any page (128 PAGE -- 256 Byte on Each Page)
+ *@Note:    The source code below will print out data from page
+            Page 0x000 100---> Test data will run from 0-250
+            Page 0x000 200---> Test data will run from 250-0
+            erase_data ((uint8_t Page_num) --> every thing on specific page
+ ************************************************************************************************/
+void task_extra_credit(void *p) {
+  const uint32_t spi_clock_mhz = 1;
+  ssp2__setup(spi_clock_mhz);
+
+  /* P1_0-CLK | P1_1-MISO | P1_4-MOSI */
+  ssp2_PIN_config();
+
+  uint8_t temp[250];
+  uint8_t temp2[250];
+  // ssp2__write_page(0x000200);
+
+  while (1) {
+    // erase_data(0x00);
+    // delay__ms(100);
+
+    fprintf(stderr, "Page 0x000 1000 \n");
+    ssp2__read_page(0x000100, temp);
+    for (int i = 0; i <= 250; i++) {
+      fprintf(stderr, "Read Data[%d]: %X \n", i, temp[i]);
+    }
+    // vTaskDelay(1000);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Page 0x000 2000 \n");
+    ssp2__read_page(0x000200, temp2);
+    for (int i = 0; i <= 250; i++) {
+      fprintf(stderr, "Read Data[%d]: %X \n", i, temp2[i]);
+    }
+    vTaskDelay(1000);
+    vTaskSuspend(NULL);
+  }
+}
+
+/***************************************** MAIN LOOP *********************************************
+**************************************************************************************************/
+int main(void) {
+  spi_bus_mutex = xSemaphoreCreateMutex();
+  puts("Starting RTOS");
+
+  /******************************* Part 1 **************************** */
+  // xTaskCreate(spi_task, "SPI Task", 4096, NULL, 1, NULL);
+
+  /******************************* Part 2B **************************** */
+  // xTaskCreate(Mutex_ID_task, "Task: 1", 1024, &task1, 1, NULL);
+  // xTaskCreate(Mutex_ID_task, "Task: 2", 1024, &task2, 1, NULL);
+
+  /****************************** Part 2 + 3 ***************************/
+  xTaskCreate(task_extra_credit, "Ex-Credit", 4096, NULL, 1, NULL);
+
+  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+
+  return 0;
 }
