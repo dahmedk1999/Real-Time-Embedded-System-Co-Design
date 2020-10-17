b2b186b89f524c8321a8edc6e9fd43722a61748b
bang nguyen <pherobang.nguyen@gmail.com>
2020-10-17 16:13:15 -0700

    update lab=WatchDog1

--- EmbeddedSystem/projects/lab8/l5_application/main.c
+++ EmbeddedSystem/projects/lab8/l5_application/main.c
@@ -17,56 +17,95 @@
 #include "task.h"
 #include <stdio.h>
 
+#include "acceleration.h"
 #include "string.h"
 #include "uart_lab.h"
+
+#include "ff.h"
 /* -------------------------------------------------------------------------- */
 /* ------------------------------- Delcaration ------------------------------ */
-typedef enum { SwitchOFF, SwicthON } switch_e;
 
-bool SW_Status_P0_29(void) {
-  gpio1__set_as_input(0, 29);
-  if (gpio1__get_level(0, 29))
-    return true;
-  else
-    return false;
+static QueueHandle_t sensor_queue;
+
+/* -------------------------- Write file to SD card ------------------------- */
+void write_file_using_fatfs_spi(void) {
+  const char *filename = "ouput.txt";
+  FIL file; // File handle
+  UINT bytes_written = 0;
+  FRESULT result = f_open(&file, filename, (FA_WRITE | FA_CREATE_ALWAYS));
+  if (FR_OK == result) {
+    char string[64];
+    sprintf(string, "Value,%i\n", 123);
+    if (FR_OK == f_write(&file, string, strlen(string), &bytes_written)) {
+    } else {
+      printf("ERROR: Failed to write data to file\n");
+    }
+    f_close(&file);
+  } else {
+    printf("ERROR: Failed to open: %s\n", filename);
+  }
+
+  // char string2[64];
+  // f_read(&file, &string2, strlen(string2), &bytes_written);
+  // puts("after\n");
+  // for (int i = 0; i < strlen(string2); i++) {
+  //   fprintf(stderr, "%c", string2[i]);
+  // }
 }
-static QueueHandle_t sw_queue;
+
 /***************************************Queue Task (Part 1)*********************************
  *@brief: Checking the behavior of Queue Receive vs Queue Send in FreeRTOS
  *@Note:  Check it with different priority between Producer vs Consumer
  ******************************************************************************************/
+typedef struct {
+  acceleration__axis_data_s sample[100];
+  double sum_y;
+  double avg_y;
+} data;
+
 /* ---------------------------------- SEND ---------------------------------- */
 static void producer_task(void *P) {
-  int counter = 0;
+  /*Initialize sensor*/
+  fprintf(stderr, "Sensor Status: %s\n", acceleration__init() ? "Ready" : "Not Ready");
+  data data1;
   while (1) {
 
-    const switch_e sw_value = SW_Status_P0_29();
-    printf("P_beforeSEND\n");
-    printf("[%d]P_afterSEND: %s\n", counter, xQueueSend(sw_queue, &sw_value, 0) ? "True" : "False");
-    counter++;
+    for (int i = 0; i < 100; i++) {
+      data1.sample[i] = acceleration__get_data();
+      data1.sum_y += data1.sample[i].y;
+    }
+    data1.avg_y = data1.sum_y / 100;
+    printf("EnQueue: %s\n", xQueueSend(sensor_queue, &data1.avg_y, 0) ? "1" : "0");
     vTaskDelay(1000);
   }
 }
 /* --------------------------------- RECEIVE -------------------------------- */
 static void consumer_task(void *P) {
-  switch_e sw_value = SwitchOFF;
-  int counter = 0;
+  const char *filename = "file.txt";
+  FIL file; // File handle
+  UINT bytes_written = 0;
+  FRESULT result = f_open(&file, filename, (FA_WRITE | FA_CREATE_ALWAYS));
+  double receive;
   while (1) {
+    if (xQueueReceive(sensor_queue, &receive, portMAX_DELAY)) {
+      if (FR_OK == result) {
+        static char string[64];
+        sprintf(string, "Avg: %f\n", receive);
+        if (FR_OK == f_write(&file, string, strlen(string), &bytes_written)) {
 
-    printf("C_beforeRECEIVE\n");
-    printf("[%d]RECEIVED: %s --> Value: %d \n", counter,
-           xQueueReceive(sw_queue, &sw_value, portMAX_DELAY) ? "True" : "False", sw_value);
-    counter++;
+          /* Save progress*/
+          f_sync(&file);
+          vTaskDelay(10);
+        } else {
+          printf("ERROR: Failed to write data to file\n");
+        }
+      } else {
+        printf("ERROR: Failed to open: %s\n", filename);
+      }
+    }
+    printf("Avg: %f\n", receive);
   }
 }
-
-/***************************************CLI Task (Part 2)*********************************
- *@brief: create your own CLI handler: taskcontrol (SUSPEND or RESUME) <Task Name >
- *@Note:  Checkout files
-          "cli_handler.h"       -- Declare Handler function
-          "sj2_cli.c"           -- Declare Handler struct inside sj2_cli__init()
-          "handlers_general.c"  -- Definition Handler function
- *****************************************************************************************/
 
 /* ------------------------------- Blink LED0 ------------------------------- */
 static void blink_task(void *P) {
@@ -85,82 +124,13 @@ int main(void) {
 
   /* ----------------------------- Initialization ----------------------------- */
   puts("Starting RTOS\n");
-
-  /* --------------------------------- Part 1a --------------------------------- */
-  /* ----------------------------- Priority: P > C ---------------------------- */
-  // sw_queue = xQueueCreate(1, sizeof(switch_e));
-  // xTaskCreate(producer_task, "producer", 2048 / sizeof(void *), NULL, 1, NULL);
-  // xTaskCreate(consumer_task, "consumer", 2048 / sizeof(void *), NULL, 2, NULL);
-
-  /*******************************Explanation: OUTPUT******************************
-  P_beforeSEND                          ==>Begin 1st (P-Task)
-  [0]P_afterSEND: True
-  C_beforeRECEIVE
-  [0]RECEIVED: True --> Value: 0        ==>Finish 1st iteration(C-Task)
-  C_beforeRECEIVE                       ==>Loopback (C-Task)->Sleep(Queue_Empty)
-  P_beforeSEND                          ==>Begin 2nd (P-Task)
-  [1]P_afterSEND: True
-  [1]RECEIVED: True --> Value: 1        ==>Finish 2nd iteration(C-Task)
-  C_beforeRECEIVE                       ==>Loopback (C-Task)->Sleep(Queue_Empty)
-  P_beforeSEND                          ==>Begin 3rd (P-Task)
-  [2]P_afterSEND: True
-  [2]RECEIVED: True --> Value: 1        ==>Finish 3rd iteration(C-Task)
-  C_beforeRECEIVE                       ==>Loopback (C-Task)->Sleep(Queue_Empty)
-  P_beforeSEND                          ==>Begin 4th (P-Task)
-  [3]P_afterSEND: True
-  [3]RECEIVED: True --> Value: 0        ==>Finish 3rd iteration(C-Task)
-  C_beforeRECEIVE
-  ********************************************************************************/
-
-  /* --------------------------------- Part 1b --------------------------------- */
-  /* ----------------------------- Priority: P < C ---------------------------- */
-  // sw_queue = xQueueCreate(1, sizeof(switch_e));
-  // xTaskCreate(producer_task, "producer", 2048 / sizeof(void *), NULL, 1, NULL);
-  // xTaskCreate(consumer_task, "consumer", 2048 / sizeof(void *), NULL, 2, NULL);
-
-  /*******************************Explanation: OUTPUT******************************
-   The output is messup due to Following these reason:
-   *
-   * -> consumer(H) run first --> However, queue is empty --> Sleep
-   * --> It wont have a chance to execute anything below
-   * ---> xQueueReceive(..,.., portMAX_DELAY) this never return <portMAX_DELAY>
-   * ----> Force consumer_task Sleep
-   *
-   * -> producer(L) run after consumer sleep
-   * --> Print before SEND
-   * ---> Push Value to Queue
-   * ----> xQueueSend(..,.., 0) return TRUE
-   *
-    C_beforeRECEIVE
-    P_beforeSEND
-    [0]RECEIVED: True ue
-    Value: 0
-    C_beforeRECEIVE
-    PC_beforeRECEIVE
-    [_beforeRECEIVE
-    True
-    _beforeRECEI[1]RECEIVED: TruTrue
-    Value: 0
-    C_beforeRECEIVE
-    [_beforeRECEIVE
-    True
-    P_beforeSEND
-    [2]RECEIVED: True --> Value: 0
-    C1]P_afterSEND: [1]P_afterSEND: True
-    P_beforeSEND
-    [3]RECEIVED: True --> Value: 0
-    C_beforeRECEIVE
-    [_beforeRECEIVE
-    True
-  ********************************************************************************/
-
-  /* --------------------------------- Part 1c --------------------------------- */
+  // write_file_using_fatfs_spi();
   /* ----------------------------- Priority: P = C ---------------------------- */
 
-  // sw_queue = xQueueCreate(1, sizeof(switch_e));
+  sensor_queue = xQueueCreate(1, sizeof(double));
 
-  // xTaskCreate(producer_task, "producer", 2048 / sizeof(void *), NULL, 1, NULL);
-  // xTaskCreate(consumer_task, "consumer", 2048 / sizeof(void *), NULL, 1, NULL);
+  xTaskCreate(producer_task, "producer", 2048 / sizeof(void *), NULL, 2, NULL);
+  xTaskCreate(consumer_task, "consumer", 2048 / sizeof(void *), NULL, 2, NULL);
 
   /*******************************Explanation: OUTPUT******************************
    * Since Two_Task have the same priority
@@ -171,8 +141,8 @@ int main(void) {
    *********************************************************************************/
 
   /* --------------------------------- Part 2 --------------------------------- */
-  sj2_cli__init();
-  xTaskCreate(blink_task, "led0", 2048 / sizeof(void *), NULL, 2, NULL);
+  // sj2_cli__init();
+  // xTaskCreate(producer_task, "xyz", 2048 / sizeof(void *), NULL, 2, NULL);
 
   vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
 
