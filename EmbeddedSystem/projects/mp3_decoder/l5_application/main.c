b09f3154ae8ec94990808516cd8eb756db484437
bang nguyen <pherobang.nguyen@gmail.com>
2020-10-30 22:56:23 -0700

    Update decoder

---
+++ EmbeddedSystem/projects/mp3_decoder/l5_application/main.c
@@ -0,0 +1,48 @@
+#include "FreeRTOS.h"
+#include "adc.h"
+#include "board_io.h"
+#include "common_macros.h"
+#include "delay.h"
+#include "gpio.h"
+
+#include "gpio_lab.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
+#include "periodic_scheduler.h"
+
+#include "oled.h"
+#include "queue.h"
+#include "semphr.h"
+#include "sj2_cli.h"
+#include "ssp2_lab.h"
+#include "task.h"
+#include <stdio.h>
+
+#include "string.h"
+#include "uart_lab.h"
+
+/* ------------------------------- OLED ------------------------------- */
+void oled_task() {
+  while (1) {
+    turn_on_lcd();
+  }
+}
+
+/***************************************** MAIN LOOP *********************************************
+**************************************************************************************************/
+int main(void) {
+
+  /* ----------------------------- Initialization ----------------------------- */
+  puts("Starting RTOS\n");
+  // sj2_cli__init();
+
+  /* --------------------------------- Part 1 --------------------------------- */
+  oled_print("->CMPE146:RTOS<-");
+
+  while (1) {
+  }
+
+  // vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+
+  return 0;
+}
