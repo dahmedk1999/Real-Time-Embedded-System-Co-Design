8e31be5b8ab4770fd9e17ed714eeb339f2a91e7e
bang nguyen <pherobang.nguyen@gmail.com>
2020-11-01 21:49:49 -0800

    Update Mp3 project ( new driver decoder + CLI work )

--- EmbeddedSystem/projects/mp3_decoder/l5_application/main.c
+++ EmbeddedSystem/projects/mp3_decoder/l5_application/main.c
@@ -18,33 +18,81 @@
 #include "task.h"
 #include <stdio.h>
 
+#include "cli_handlers.h"
 #include "decoder_mp3.h"
+#include "ff.h"
 #include "string.h"
 #include "uart_lab.h"
 
-/* ------------------------------- OLED ------------------------------- */
-void oled_task() {
+QueueHandle_t Q_songdata;
+QueueHandle_t Q_trackname;
+TaskHandle_t player_handle;
+
+void mp3_reader_task(void *p) {
+  trackname_t name;
+  char bytes_512[512];
+  UINT br;
   while (1) {
-    turn_on_lcd();
+    xQueueReceive(Q_trackname, name, portMAX_DELAY);
+    // printf("Received song to play: %s\n", name);
+
+    // open file
+    const char *filename = name; // check if accurate
+    FIL file;
+    FRESULT result = f_open(&file, filename, (FA_READ));
+    if (FR_OK == result) {
+      while (br != 0) {
+        // read 512 bytes from file to bytes_512
+        f_read(&file, bytes_512, sizeof(bytes_512), &br);
+        xQueueSend(Q_songdata, bytes_512, portMAX_DELAY);
+
+        if (uxQueueMessagesWaiting(Q_trackname)) {
+          // printf("New play song request\n");
+          break;
+        }
+      }
+      // close file
+      f_close(&file);
+    } else {
+      // printf("Failed to open mp3 file \n");
+    }
   }
 }
 
-/***************************************** MAIN LOOP *********************************************
-**************************************************************************************************/
+void mp3_player_task(void *p) {
+  int counter = 1;
+  while (1) {
+    char bytes_512[512];
+    xQueueReceive(Q_songdata, bytes_512, portMAX_DELAY);
+    for (int i = 0; i < 512; i++) {
+      // printf("%x \t", bytes_512[i]);
+      while (!gpio1__get_level(2, 0)) {
+        vTaskDelay(1);
+      }
+      decoder_send_mp3Data(bytes_512[i]);
+    }
+    // printf("Received 512 bytes : %d\n", counter);
+    counter++;
+  }
+}
+
 int main(void) {
 
-  /* ----------------------------- Initialization ----------------------------- */
-  puts("Starting RTOS\n");
+  Q_trackname = xQueueCreate(1, sizeof(trackname_t));
+  /* Send 512 byte at once */
+  Q_songdata = xQueueCreate(1, 512);
+
+  // enable GPIO interrupt
+  // init_SPI();
+  // mp3_setup();
   decoder_setup();
-  // sj2_cli__init();
 
-  /* --------------------------------- Part 1 --------------------------------- */
-  oled_print("->CMPE146:RTOS<-");
+  sj2_cli__init();
 
-  while (1) {
-  }
+  xTaskCreate(mp3_reader_task, "reader", (2024 * 4) / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
+  xTaskCreate(mp3_player_task, "player", (3096 * 4) / sizeof(void *), NULL, PRIORITY_MEDIUM,
+              &player_handle); // made a change here
 
-  // vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
-
+  vTaskStartScheduler();
   return 0;
 }
